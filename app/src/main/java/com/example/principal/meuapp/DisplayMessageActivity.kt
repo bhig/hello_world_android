665907b06cb9592cf655d4d0017d698b98f40d40
Flavio Cesar Faria de Lima <flaviocesar_lima@hotmail.com>
2018-10-13 00:10:54 -0300

    Desafio do calculo

--- app/src/main/java/com/example/principal/meuapp/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/principal/meuapp/DisplayMessageActivity.kt
@@ -1,11 +1,13 @@
 package com.example.principal.meuapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
 
 class DisplayMessageActivity : AppCompatActivity() {
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_message)
@@ -17,7 +19,21 @@ class DisplayMessageActivity : AppCompatActivity() {
         val textView = findViewById<TextView>(R.id.textView)
         if (error.isEmpty()) {
             val result = intent.getIntExtra(RESULT, 0)
-            textView.text = result.toString()
+            val challenge = intent.getIntExtra(CHALLENGE, 0)
+            val challengeCalculate = intent.getStringExtra(CHALLENGE_CALCULATE)
+
+            var finalResult = "$challengeCalculate = $challenge\n"
+            finalResult += "\nMeu palpite = $result"
+
+            if (result == challenge) {
+                for (x in 1..10) {
+                    finalResult += "\nVocê acertou!"
+                }
+            } else {
+                finalResult += "\nVocê errou!"
+            }
+
+            textView.text = finalResult
 
         } else {
             textView.text = error
--- app/src/main/java/com/example/principal/meuapp/MainActivity.kt
+++ app/src/main/java/com/example/principal/meuapp/MainActivity.kt
@@ -1,38 +1,60 @@
 package com.example.principal.meuapp
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.EditText
-import java.lang.Exception
+import android.widget.TextView
 
 const val RESULT = "message.result"
+const val CHALLENGE = "message.challenge"
 const val ERROR = "message.error"
+const val CHALLENGE_CALCULATE = "message.challenge.calculate"
 
+@SuppressLint("SetTextI18n")
 class MainActivity : AppCompatActivity() {
+
+    private var firstNumber = 1
+    private var secondNumber = 1
+    private var challengeLabel = "Aperte o botão"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.challenge)
+        textView.text = challengeLabel
     }
 
-    /**Called when the user taps the Send button */
+    fun startChallenge(view: View) {
+        firstNumber = (1..20).shuffled().first()
+        secondNumber = (1..20).shuffled().first()
+
+        val textView = findViewById<TextView>(R.id.challenge)
+        challengeLabel = "$firstNumber x $secondNumber"
+        textView.text = "$challengeLabel = ?"
+    }
+
     fun sendMessage(view: View) {
-        val intent = Intent(this, DisplayMessageActivity::class.java)
-        val editText = findViewById<View>(R.id.editText) as EditText
+        val challengeMessage = findViewById<View>(R.id.challenge) as TextView
+        if (challengeMessage.text != "Aperte o botão") {
 
-        try {
-            val result = editText.text.toString().toInt()
-            intent.putExtra(RESULT, result)
-            intent.putExtra(ERROR, "")
+            val intent = Intent(this, DisplayMessageActivity::class.java)
+            val editText = findViewById<View>(R.id.editText) as EditText
+            try {
+                val result = editText.text.toString().toInt()
+                intent.putExtra(RESULT, result)
+                intent.putExtra(ERROR, "")
+                intent.putExtra(CHALLENGE, firstNumber * secondNumber)
+                intent.putExtra(CHALLENGE_CALCULATE, challengeLabel)
 
-        } catch (e: Exception) {
-            intent.putExtra(ERROR, "Error: Insira apenas número!")
+            } catch (e: Exception) {
+                intent.putExtra(ERROR, "Error: Insira apenas número!")
+            }
+
+            startActivity(intent)
         }
-
-        startActivity(intent)
     }
-
-
 }
